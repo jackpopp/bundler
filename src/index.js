0039ca2c1726b2068558ebbe19ad9aad8a61c1fb
Jack Hannigan Popp <jack.popp@hotmail.com>
2018-09-10 13:59:33 +0100

    Refactor process check

--- src/index.js
+++ src/index.js
@@ -20,6 +20,10 @@ function cjsTemplate(id, source) {
     });`;
 }
 
+function iifeTemplate(source) {
+    return `(function() {${source}}())`;
+}
+
 function sourceToAST(source) {
     return acorn.parse(source, {
         ecmaVersion: 2018,
@@ -29,8 +33,9 @@ function sourceToAST(source) {
 
 // root path should be where the bundler was invoked from
 // source path should be where the inital file was loaded from
-function walkAndParse(ast, currentPath) {
-    walk.simple(ast, {
+function walkAndParse(ast, currentPath, resolver) {
+    
+    walk.ancestor(ast, {
         CallExpression(node) {
             if (node.callee.type === 'Identifier' && node.callee.name === 'require' && node.arguments[0] && node.arguments[0] && node.arguments[0].type === 'Literal') {
                 const modulePath = node.arguments[0].value;
@@ -61,7 +66,7 @@ function walkAndParse(ast, currentPath) {
                     resolvedModule: resolved,
                     modulePath: modulePath,
                     fullPath: fullPath,
-                    exports: parseExports(resolved, newResolutionPath, id),
+                    exports: parseExports(resolved, newResolutionPath, id, resolver),
                     id: id
                 }
 
@@ -72,9 +77,63 @@ function walkAndParse(ast, currentPath) {
                 }
             }
         },
-        IfStatement(node) {
-            const { left, right } = node.test;
-            process.exit(0);
+        IfStatement(node) { 
+            // This mod allows us to generate different bundles based on the NODE_ENV value, allowing develpoment and production builds
+            const { left, right, operator } = node.test;
+            // needs to be a binary expression
+            // check if either left or right are memeber expressions that look for the node env
+            // check if either the left or right string literal, if it is lets build and compare the two
+            if (node.test.type !== 'BinaryExpression') return;
+
+            if ((left.type === 'MemberExpression' || right.type === 'MemberExpression') && (left.type === 'Literal' || right.type === 'Literal')) {
+                const member = right.type === 'MemberExpression' ? right : left;
+                const literal = right.type === 'Literal' ? right : left;
+
+                // JS is evaulated right to left so `env` object with be acessible on the first object key, `processs` on the next, even though its coded the other way around
+                if (member.object.type !== 'MemberExpression' || member.object.object === undefined || member.object.object.name !== 'process') return;
+                if (member.object.property.name !== 'env' && member.property.name !== 'NODE_ENV') return;
+
+
+                const { value } = literal;
+                // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function
+                const result = new Function(`return "${value}" ${operator} "${process.env.NODE_ENV}"`)();
+                
+
+                // this is kinda nasty but you should have seen how I was trying to achieve this effect before 
+                // https://github.com/jackpopp/bundler/blob/master/src/index.js#L183 mega hack and both versions of the scripts we bundled but only one executed
+                const newTest = sourceToAST('true');
+                node.test = newTest.body[0].expression;
+
+                // if result is true then take the consequent and set as the node
+                // if result is false and there is a alternate set that as the body
+                // if nothing set the node as block statement
+                /*if (result === true) {
+                    console.log('consequent')
+                    node = node.consequent.body;
+                    return
+                }
+
+                if (result === false && node.alternate && node.alternate.body) {
+                    console.log('alternate');
+                    node = node.alternate.body;
+                    return
+                }
+
+                console.log('nullify');
+                node = null;*/
+                if (result === true && node.alternate && node.alternate.body) {
+                    node.alternate.body = [];
+                }
+
+                if (result === false) {
+                    if (node.alternate && node.alternate.body) {
+                        node.consequent.body = node.alternate.body;
+                        return;
+                    }
+
+                    node.consequent.body = [];
+                }
+            }
         }
     });
 
@@ -87,10 +146,10 @@ function walkAndParse(ast, currentPath) {
  * Walks the AST and parers any call expressions for require function calls
  */
 
-function parseExports(resolvedSource, currentPath, id) {
+function parseExports(resolvedSource, currentPath, id, resolver) {
     const wrappedSource = cjsTemplate(id, resolvedSource);
     const resolvedAST = sourceToAST(wrappedSource);
-    return walkAndParse(resolvedAST, currentPath);
+    return walkAndParse(resolvedAST, currentPath, resolver);
 }
 
 function generateCode(init, graph) {
@@ -100,35 +159,23 @@ function generateCode(init, graph) {
 }
 
 const start = process.hrtime();
-const ROOT_PATH = `${process.cwd()}/demo/`;
-const PATH = `${ROOT_PATH}/a.js`;
-const resolver = new Resolver(ROOT_PATH);
-const init = fs.readFileSync(require.resolve(PATH));
-const initalAST = sourceToAST(init);
-const parsedIntialAST = walkAndParse(initalAST, ROOT_PATH);
-const code = generateCode(parsedIntialAST, GRAPH);
 
-/*
-    lol this need to go in the parser checking if statements and removing the block
-    lol this need to go in the parser checking if statements and removing the block
-    visit the if statement
-    if the check is to check the node env process 
-    then check what mode we're in 
-    if any dont match the statements and we find a require call within 
-    then mark it
-*/
-const processWrapper = `
-    window.process = {
-        env: {
-            NODE_ENV: 'development'
-        }
-    };
-    process.env.NODE_ENV;`;
+function bundler(entryPoint) {
+    const ROOT_PATH = `${process.cwd()}/demo/`;
+    const PATH = `${ROOT_PATH}/a.js`;
+    const resolver = new Resolver(ROOT_PATH);
+    const init = fs.readFileSync(require.resolve(PATH), 'UTF-8');
+    const initalAST = sourceToAST(init);
+    const parsedIntialAST = walkAndParse(initalAST, ROOT_PATH, resolver);
+    const code = generateCode(parsedIntialAST, GRAPH);
 
-const iffe = `(function() {${processWrapper}\n${code}}())`;
+    return iifeTemplate(code);
 
-fs.writeFileSync(`${__dirname}/generated.js`, iffe, 'UTF-8')
+   
+}
 
+const code = bundler('');
+fs.writeFileSync(`${__dirname}/generated.js`, code, 'UTF-8')
 
 const end = process.hrtime(start);
 console.log(`Took: ${end[0]}s ${end[1] / 1000000}ms`);
